22e27dda933b960356a4cf0584868d6ff97ac597
andraObrien07 <comanan09@craighead.school.nz>
2023-06-14 11:05:58 +1200

    44

--- code/2-conversation.js
+++ code/2-conversation.js
@@ -3,19 +3,21 @@
 // ####################################
 
 function conversation() {
-  alert('Welcome to my conversation program.')
+  alert("Let's pick a sport for you.")
 
-  alert('Do you like cycling? Answer yes or no.')
+  alert('Do you like running? Answer yes or no.')
   let answer = prompt()
 
   if (answer == 'yes') {
-    alert("That's good - you will get very fit.")
+    alert("That's good - you should try track and field sports.")
+    alert('Do you like track and field? Answer yes or no.')
+    let answer = prompt()
+    alert('You should do it then!')
   } else {
     alert('Perhaps you like some other sport.')
   }
-
-  alert('Goodbye')
 }
+
 
 function age() {
   // write your code here
