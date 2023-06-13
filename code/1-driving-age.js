61f37ec04949506a6ca0240d678fcbe96277ec9e
andraObrien07 <comanan09@craighead.school.nz>
2023-06-14 10:39:32 +1200

    you can drive i cant

--- code/1-driving-age.js
+++ code/1-driving-age.js
@@ -3,9 +3,13 @@
 // ####################################
 
 function drivingAge() {
-  let age = 18
+  let age = prompt('')
 
   if (age > 16) {
     alert("You're old enough to drive.")
+  }
+
+  else {
+    alert('You are not old enough to drive')
   }
 }
