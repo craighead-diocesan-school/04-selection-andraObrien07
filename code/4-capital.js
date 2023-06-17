e92f3fc83cbbc8ef9c3226cfff2f61a2ac9f57c0
andraObrien07 <comanan09@craighead.school.nz>
2023-06-17 17:01:22 +1200

    i tried
    then gave up. temporaroly

--- code/4-capital.js
+++ code/4-capital.js
@@ -5,12 +5,13 @@
 function capital() {
   // write your code here
   let name = prompt('What is your name?')
-  let capital = prompt(name + 'What is the capital of france')
+  let capital = prompt(name + ' What is the capital of france')
 
-  if (captial == 'Paris') {
+  if (capital == 'Paris') {
     alert('That is correct ! Good job!')
   }
-  else if (captial != 'Paris') {
+  else if (capital != 'Paris') {
     alert('That wrong loser ahahhaha')
+    alert("If you answered 'paris' you should try with a capital letter.")
   }
 }
--- code/5-sale-calculator.js
+++ code/5-sale-calculator.js
@@ -4,4 +4,31 @@
 
 function saleCalculator() {
   // write your code here
+
+  let money = prompt('How much money do you have?')
+  let price = prompt('How much is the price of the item you want to buy?')
+  let discount = prompt('What is the discount percentage? for that item')
+
+  money = Number(money)
+  price = Number(price)
+  discount = Number(discount)
+  decimaldiscount = discount / 100
+  decimaldiscount = Number(decimaldiscount)
+  NewTotal = decimaldiscount * price
+
+  // if (money < NewTotal) {
+  //   alert('You do not have enough money!')
+  // }
+  if (money < price) {
+    alert('You do not have enough money ')
+  }
+
+  else if (money > price) {
+    alert('You have enough money.')
+
+  }
+
+
+
+
 }
