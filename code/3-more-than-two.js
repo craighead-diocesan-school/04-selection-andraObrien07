7e6e13ebf8341640919045020fdd055e22f47713
andraObrien07 <comanan09@craighead.school.nz>
2023-06-29 10:29:55 +1200

    idk

--- code/3-more-than-two.js
+++ code/3-more-than-two.js
@@ -5,9 +5,9 @@
 function moreThanTwo() {
   let stop = prompt('Do u wanna stop')
 
-  // let number = prompt('Give me a number')
+
   while (stop != 'yes') {
-    // let number = prompt('Give me a number')
+
     let number = prompt('Give me a number')
     if (number > 2) {
       alert(number + ' is more than 2')
@@ -17,8 +17,7 @@ function moreThanTwo() {
       alert(number + ' is less than 2')
     }
 
-    // let stop = prompt('Do u wanna stop')
 
   }
-  // let no = go
+
 }
