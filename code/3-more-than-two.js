28c000f6b98f9057e660702ffb28b13fffff6cc3
andraObrien07 <comanan09@craighead.school.nz>
2023-07-15 14:37:14 +1200

    the repeat loop doesnt work

--- code/3-more-than-two.js
+++ code/3-more-than-two.js
@@ -5,24 +5,38 @@
 function moreThanTwo() {
   // let stop = prompt('Do u wanna stop')
 
-  Alert('is the number more than 2?')
-  let number = prompt('Give me a number')
-  let stop = prompt('Do u wanna stop')
-  while (stop != 'yes') {
+  // Alert('is the number more than 2?')
+  // let number = prompt('Give me a number')
+  // let stop = prompt('Do u wanna stop')
+  // while (stop != 'yes') {
 
+  //   let number = prompt('Give me a number')
+  //   let stop = prompt('Do u wanna stop')
+  //   if (number > 2) {
+  //     alert(number + ' is more than 2')
+  //   }
+
+  //   else if (number < 2) {
+  //     alert(number + ' is less than 2')
+  //   }
+
+  //   else {
+  //     let stop = prompt('Do u wanna stop')
+  //   }
+  // }
+  let number = prompt('Give me a number')
+
+  while (number != 'stop') {
     let number = prompt('Give me a number')
-    let stop = prompt('Do u wanna stop')
     if (number > 2) {
       alert(number + ' is more than 2')
+    }
+    else if (number == 2) {
+      alert('thats 2')
     }
 
     else if (number < 2) {
       alert(number + ' is less than 2')
     }
-
-    else {
-      let stop = prompt('Do u wanna stop')
-    }
   }
-
 }
